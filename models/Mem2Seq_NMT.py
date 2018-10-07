5371ec0468229297401edfeb6b281c75b4133544
Zen Tang <ztang230@gmail.com>
2018-10-07 19:58:52 -0400

    change to item() - next_in

--- models/Mem2Seq_NMT.py
+++ models/Mem2Seq_NMT.py
@@ -188,8 +188,7 @@ class Mem2Seq(nn.Module):
             all_decoder_outputs_ptr[t] = decoder_ptr
             _, toppi = decoder_ptr.data.topk(1)
             top_ptr_i = torch.gather(input_batches,0,Variable(toppi.view(1, -1)))
-            next_in = [top_ptr_i.squeeze()[i].data[0] if(toppi.squeeze()[i] < input_lengths[i]-1) else topvi.squeeze()[i] for i in range(batch_size)]
-
+            next_in = [top_ptr_i.squeeze()[i].item() if(toppi.squeeze()[i] < input_lengths[i]-1) else topvi.squeeze()[i] for i in range(batch_size)]
             decoder_input = Variable(torch.LongTensor(next_in)) # Chosen word is next input
             if USE_CUDA: decoder_input = decoder_input.cuda()
 
